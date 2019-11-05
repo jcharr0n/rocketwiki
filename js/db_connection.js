52f1fd07b76b49a10f3b9401314a47f728a1e40c
Jackson <jmc2817@g.rit.edu>
2019-11-05 00:09:52 -0500

    disclaimer lol

--- js/db_connection.js
+++ js/db_connection.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
 
+// i know its bad practice to have this file publicly visible.
+// there is nothing in the db right now though
 var con = mysql.createConnection({
   host: "localhost",
   user: "debian-sys-maint",
